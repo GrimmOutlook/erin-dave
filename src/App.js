d4dc66b890a51f99bc99fdfc80b0ce14296de909
GrimmOutlook <grimmoutlook87@gmail.com>
2019-01-12 20:58:18 -0500

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => (
+    <React.Fragment>
+      <div className="flex__container">
+          <div className="img__container">
+              <img src="/dist/img/top-right.jpg" />
+          </div>
+          <div className="save__date">
+            <div className="save__date__text">
+              <h1>
+              Save The Date!!
+              </h1>
+              <h2>September 7, 2019</h2>
+              <h3>Columbia, Maryland</h3>
+              <h3>More Details to Follow!</h3>
+            </div>
+          </div>
       </div>
-    );
-  }
-}
+      <p>Wedding Stuff</p>
+    </React.Fragment>
+  );
 
 export default App;
